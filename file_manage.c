c15dec2cec2559cd8ea7d63e5e1aa3cf030050b6
rafael <tandu.rafael@gmail.com>
2020-11-22 22:17:01 +0100

    reading and storage of data completed

--- file_manage.c
+++ file_manage.c
@@ -12,6 +12,7 @@
 #define E34_MAX      16 // Tableau de taille 10000`
 #define F0_MAX       128 // Tableau de taille 10000`
 #define GDF_MAX      100 // Tableau de taille 10000`
+#define METHOD_MAX      100 // Tableau de taille 10000`
 #define SA_MAX       90 // Tableau de taille 10000`
 #define FILE_NUMBER  10000 // Tableau de taille 10000`
 #define CLASS_NUMBER 9 // Tableau de taille 10000`
@@ -34,7 +35,6 @@ void get_file_names(const char* dirpath, char** files)
   
  	char* dir1= (char *) malloc(1 + strlen(dirpath));
 	strcpy(dir1,dirpath);
-	printf("dir %s\n", dir1);
 	    	
 
   d = opendir(dir1);
@@ -78,13 +78,11 @@ int read_file(char* filepath, char* line, char* method,struct object *data)
 {
 	FILE* file = NULL;
 	
-	//filepath = finalpath;
-	// Check file existence
+	
 	int returnval;
 	returnval = access (filepath, F_OK);
 	if (returnval == 0)
 	{
-		//printf ("\n %s exists\n", strvar);
 		int returnval;
 	    file = fopen(filepath, "r");
 	 	
@@ -94,7 +92,7 @@ int read_file(char* filepath, char* line, char* method,struct object *data)
 	        while(fgets(line, TAILLE_MAX, file)) // On lit maximum TAILLE_MAX caractères du fichier, on stocke le tout dans "chaine"
 	       	{	
 	       		store_data(line,data, method,&i);
-	       		printf("%s", line); // On affiche la chaîne 
+	       		//printf("%s", line); // On affiche la chaîne 
 	        	i++;	
 	        }
 
@@ -105,6 +103,7 @@ int read_file(char* filepath, char* line, char* method,struct object *data)
 	    {
 	       // On affiche un message d'erreur si on veut
 	       printf("Impossible d'ouvrir le fichier test.txt");
+	       exit(1);
 	    }
 	}
 	else
@@ -122,17 +121,23 @@ int store_data(char* line,  struct object *data, char *method, int *i)
 {
 	if (strcmp(method, "E34") == 0) 
 	{
-	 
+	// printf("METHOD E34gdb --version");
 	  data->e34[*i] = atof(line);
 	} 
 	else if (strcmp(method, "F0") == 0)
 	{
+	//	printf("METHOD  F0");
+		data->f0[*i] = atof(line);
 	}
 	else if (strcmp(method, "GFD") == 0)
 	{
+	//	 printf("METHOD  GFD");
+		data->gdf[*i] = atof(line);
 	}
 	else if (strcmp(method, "SA") == 0)
 	{
+	//	printf("METHOD SA");
+		data->sa[*i] = atof(line);
 	}
 	/* more else if clauses */
 	else /* default: */
@@ -154,30 +159,48 @@ int main(int argc, char *argv[])
 	char* separator = "/";
 	
 	char* format= (char *) malloc(1 + strlen(dirpath[0])+ strlen(separator));
-	strcpy(format,".");
-	strcat(format, dirpath[0]);
-	char* dir = (char *) malloc(1 + strlen(dirpath[0])+ strlen(separator));	
+	
+
+	char* dir = (char *) malloc(1 + strlen(dirpath[0])+ strlen(separator));
+	int k =0;
+
 	strcpy(dir, dirpath[0]);
 	strcat(dir, separator);
 	get_file_names(dir,filesname);
-	char * finalpath = (char *) malloc(1 + strlen(dir)+ sizeof(filesname[0])+strlen(format));
-
-	strcpy(finalpath, dir);
-	strcat(finalpath, filesname[0]);
-	strcat(finalpath, format);
-
-	data[0].name = filesname[0];
 	
-	int a =read_file(finalpath,chaine,dirpath[0],&data[0]);
-	int i = 0;
-	printf("Name : %s\n", data[0].name); // On affiche la chaîne 
-	
-
-	i=0;
-	while(i < E34_MAX)
+	while(k < 4)
 	{
-		printf("data : %f\n", data[0].e34[i]); // On affiche la chaîne 
-		i++;
+		strcpy(dir, dirpath[k]);
+		strcat(dir, separator);
+
+		int i=0;
+		int j=0;
+		while(filesname[i]!= NULL)
+		{
+
+			char * finalpath = (char *) malloc(1 + strlen(dir)+ sizeof(filesname[i])+strlen(format));
+			strcpy(finalpath, dir);
+			strcat(finalpath, filesname[i]);
+			strcpy(format,".");
+			strcat(format, dirpath[k]);
+			strcat(finalpath, format);
+
+			data[i].name = filesname[i];
+			int a =read_file(finalpath,chaine,dirpath[k],&data[i]);
+			
+
+			j++;
+			i++;
+		}
+
+		k++;
 	}
+
+	printf("Name : %s\n", data[0].name); // On affiche la chaîne 
+	printf("e34  : %f\n", data[0].e34[0]); // On affiche la chaîne 
+	printf("f0   : %f\n", data[0].f0[0]); // On affiche la chaîne
+	printf("gdf  : %f\n", data[0].gdf[0]); // On affiche la chaîne
+	printf("sa   : %f\n", data[0].sa[3]); // On affiche la chaîne 
+		
     return 0;
 }
